d952ccd56bb5e0c87c36eec996ccbf9ccd57807f
Aleksey <alex.stolpovsky@yandex.ru>
2020-02-17 22:10:17 +0300

    add textwatcher

--- app/src/main/java/com/citizen/moneytracker/AddItemActivity.java
+++ app/src/main/java/com/citizen/moneytracker/AddItemActivity.java
@@ -3,12 +3,63 @@ package com.citizen.moneytracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+import org.w3c.dom.Text;
 
 public class AddItemActivity extends AppCompatActivity {
+
+    private EditText name;
+    private EditText price;
+    private Button addBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_additem);
+
+        name = findViewById(R.id.name);
+        price = findViewById(R.id.price);
+        addBtn = findViewById(R.id.add_btn);
+
+        TextWatcher editWatcher = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (name.getText().toString().trim().length() <= 0 ||
+                        price.getText().toString().trim().length() <= 0)
+                    addBtn.setEnabled(false);
+                else
+                    addBtn.setEnabled(true);
+            }
+        };
+
+        name.addTextChangedListener(editWatcher);
+        price.addTextChangedListener(editWatcher);
+
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String itemName = name.getText().toString();
+                String itemPrice = price.getText().toString();
+            }
+        });
     }
+
+
 }
